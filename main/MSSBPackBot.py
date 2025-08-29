a30d5e9698840a8bb28ef85a2fa2df9a2cc403b2
Clutch1908 <109238342+Clutch1908@users.noreply.github.com>
2025-08-29 12:00:54 -0500

    Update MSSBPackBot.py

--- main/MSSBPackBot.py
+++ main/MSSBPackBot.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 from services import MSSBCardGenerator
 from services import MSSBCardInfo
 from services import MSSBOddsInfo
+from services import MSSBPackBattle
 
 #declare Server ID for where bot is running
 GUILD_ID = discord.Object(id=1408155647987548332)
@@ -28,42 +29,182 @@ class Client(commands.Bot):
 #initialize slash commands           
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 client = Client(command_prefix="!", intents=intents)     
-
+    
 #command to return card information
 @client.tree.command(name="cardinfo", description="Return information for a specific card", guild=GUILD_ID)
-#@app_commands.checks.cooldown(1, 5, key=lambda i:(i.user.id))
 async def cardInfo(interaction: discord.Interaction, card:str):
     embed, file = MSSBCardInfo.construct_discord_embed(card)
     if embed == "notfound":
-        await interaction.response.send_message(f'Sorry, but I couldn\'t find that Card. Could you please verify your spelling and ensure there are no spaces in your input? I can take a look again once you do!')
+        await interaction.response.send_message(f'Sorry, but I couldn\'t find that Card. Could you please verify your spelling and ensure there are no spaces in your input? I can take a look again once you do!', ephemeral=True)
     else:
         embed.set_author(name=interaction.user.name, icon_url=interaction.user.display_avatar.url)
         #send Discord embed
         await interaction.response.send_message(file=file,embed=embed) 
 
+#command to open a single pack
 @client.tree.command(name="openpack", description="Opens a single pack", guild=GUILD_ID)
-#@app_commands.checks.cooldown(1, 5, key=lambda i:(i.user.id))
 async def openPack(interaction: discord.Interaction):
     embed, file = MSSBCardGenerator.openPack()
     embed.set_author(name=interaction.user.name, icon_url=interaction.user.display_avatar.url)
     await interaction.response.send_message(file=file, embed=embed)
 
+#command to return the odds of each individual card
 @client.tree.command(name="oddsinfo", description="See list of current cards with pull odds", guild=GUILD_ID)
-#@app_commands.checks.cooldown(1, 5, key=lambda i:(i.user.id))
 async def buildOdds(interaction: discord.Interaction):
     #build message that will iterate through each MSSBCardDatabase dictionary to show the odds of drawing cards
     oddsMessage, oddsMessage_2, oddsMessage_3 = MSSBOddsInfo.construct_discord_message()
-    print(oddsMessage)
-    print(oddsMessage_2)
-    print(oddsMessage_3)
-    await interaction.response.send_message(oddsMessage)
-    await interaction.followup.send(oddsMessage_2)
-    await interaction.followup.send(oddsMessage_3)
+    await interaction.response.send_message(oddsMessage, ephemeral=True)
+    await interaction.followup.send(oddsMessage_2, ephemeral=True)
+    await interaction.followup.send(oddsMessage_3, ephemeral=True)
 
-#@client.tree.error
-#async def on_app_command_error(error: app_commands.AppCommandError, interaction: discord.Interaction):
-    #if isinstance(error, app_commands.CommandOnCooldown):
-        #await interaction.response.send_message(f"Wait for {error.retry_after} seconds, I'm only a one Toad shop!!!, ephemeral = True")
+#class to set the buttons to be utilized in the follow up to Pack Battle outputs (for the recepient of the challenge)
+class ViewRerollR(discord.ui.View):
+    #set __init__ to store interaction IDs
+    def __init__(self, challengerID, challengerName, recipientID, recipientName, timeout=300):
+        super().__init__(timeout=timeout)
+        self.challengerID = challengerID
+        self.challengerName = challengerName
+        self.recipientID = recipientID
+        self.recipientName = recipientName
 
+    @discord.ui.button(label="Yes", style = discord.ButtonStyle.success, emoji="✅")
+    async def yes_button_callback_Reroll(self, interaction: discord.Interaction, button:discord.ui.button):
+        if interaction.user.id != self.recipientID:
+            await interaction.response.send_message("Hey get your own Reroll card grubby!", ephemeral=True)
+        else:
+            await interaction.response.edit_message(content="Great! Let me get you your new set of packs now...", view=None)
+            embed_rr, file_rr, rerollOpened_rr, tradeOpened_rr = MSSBCardGenerator.openMultiplePacks(5)
+            embed_rr.set_author(name=interaction.user.name, icon_url=interaction.user.display_avatar.url)
+            if rerollOpened_rr and not tradeOpened_rr:
+                await interaction.followup.send(file=file_rr, embed=embed_rr)
+                view = ViewRerollR(challengerID=self.challengerID, challengerName = self.challengerName, recipientID = self.recipientID, recipientName = self.recipientName)
+                await interaction.followup.send("Hey I see you ended up with a Reroll card there. Would you like to give me all your cards back for 5 new packs?", view=view)
+            elif (not rerollOpened_rr) and tradeOpened_rr:
+                await interaction.followup.send(file=file_rr, embed=embed_rr)
+            elif rerollOpened_rr and tradeOpened_rr:
+                await interaction.followup.send(file=file_rr, embed=embed_rr)
+            else:
+                await interaction.followup.send(file=file_rr, embed=embed_rr)
+
+    @discord.ui.button(label="No", style = discord.ButtonStyle.danger, emoji="❌")
+    async def no_button_callback_Reroll(self, interaction: discord.Interaction, button:discord.ui.button):
+        if interaction.user.id != self.recipientID:
+            await interaction.response.send_message("Hey get your own Reroll card grubby!", ephemeral=True)
+        else:
+            await interaction.response.edit_message(content="Glad you like what you have!", view=None)
+
+#class to set the buttons to be utilized in the follow up to Pack Battle outputs (for the challenger of the challenge)
+class ViewRerollC(discord.ui.View):
+    #set __init__ to store interaction IDs
+    def __init__(self, challengerID, challengerName, recipientID, recipientName, timeout=300):
+        super().__init__(timeout=timeout)
+        self.challengerID = challengerID
+        self.challengerName = challengerName
+        self.recipientID = recipientID
+        self.recipientName = recipientName
+
+    @discord.ui.button(label="Yes", style = discord.ButtonStyle.success, emoji="✅")
+    async def yes_button_callback_Reroll(self, interaction: discord.Interaction, button:discord.ui.button):
+        if interaction.user.id != self.challengerID:
+            await interaction.response.send_message("Hey get your own Reroll card grubby!", ephemeral=True)
+        else:
+            await interaction.response.edit_message(content="Great! Let me get you your new set of packs now...", view=None)
+            embed_cr, file_cr, rerollOpened_cr, tradeOpened_cr = MSSBCardGenerator.openMultiplePacks(5)
+            embed_cr.set_author(name=interaction.user.name, icon_url=interaction.user.display_avatar.url)
+            if rerollOpened_cr and not tradeOpened_cr:
+                await interaction.followup.send(file=file_cr, embed=embed_cr)
+                view = ViewRerollC(challengerID=self.challengerID, challengerName = self.challengerName, recipientID = self.recipientID, recipientName = self.recipientName)
+                await interaction.followup.send("Hey I see you ended up with a Reroll card there. Would you like to give me all your cards back for 5 new packs?", view=view)
+            elif (not rerollOpened_cr) and tradeOpened_cr:
+                await interaction.followup.send(file=file_cr, embed=embed_cr)
+            elif rerollOpened_cr and tradeOpened_cr:
+                await interaction.followup.send(file=file_cr, embed=embed_cr)
+            else:
+                await interaction.followup.send(file=file_cr, embed=embed_cr)
+
+    @discord.ui.button(label="No", style = discord.ButtonStyle.danger, emoji="❌")
+    async def no_button_callback_Reroll(self, interaction: discord.Interaction, button:discord.ui.button):
+        if interaction.user.id != self.challengerID:
+            await interaction.response.send_message("Hey get your own Reroll card grubby!", ephemeral=True)
+        else:
+            await interaction.response.edit_message(content="Glad you like what you have!", view=None)
+
+#class to set the buttons to be utilized by the Pack Battle command
+class View(discord.ui.View):
+    #set __init__ to store interaction IDs
+    def __init__(self, challengerID, challengerName, recipientID, recipientName, timeout=300):
+        super().__init__(timeout=timeout)
+        self.challengerID = challengerID
+        self.challengerName = challengerName
+        self.recipientID = recipientID
+        self.recipientName = recipientName
+        
+    @discord.ui.button(label="Yes", style = discord.ButtonStyle.success, emoji="✅")
+    async def yes_button_callback(self, interaction: discord.Interaction, button:discord.ui.button):
+        #check if user has the id of the recipient of the challenge
+        if interaction.user.id != self.recipientID:
+            await interaction.response.send_message("Hey you can't speak for them! Only the user being challenged can accept or decline the Pack Battle challenge.", ephemeral=True)
+        else:
+            await interaction.response.edit_message(content="Pack Battle Accepted!",view=None)
+            await interaction.followup.send("Pack Battle Initiated! Ooo I'm excited let me get you your packs...")
+            #open packs for the user challenged
+            embed_r, file_r, rerollOpened_r, tradeOpened_r = MSSBCardGenerator.openMultiplePacks(5)
+            embed_r.set_author(name=interaction.user.name, icon_url=interaction.user.display_avatar.url)
+            print(rerollOpened_r, tradeOpened_r)
+            if rerollOpened_r  and not tradeOpened_r:
+                await interaction.followup.send(file=file_r, embed=embed_r)
+                view = ViewRerollR(challengerID=self.challengerID, challengerName = self.challengerName, recipientID = self.recipientID, recipientName = self.recipientName)
+                await interaction.followup.send("Hey I see you ended up with a Reroll card there. Would you like to give me all your cards back for 5 new packs?", view=view)
+            elif (not rerollOpened_r) and tradeOpened_r:
+                await interaction.followup.send(file=file_r, embed=embed_r)
+            elif rerollOpened_r and tradeOpened_r:
+                await interaction.followup.send(file=file_r, embed=embed_r)
+                view = ViewRerollR(challengerID=self.challengerID, challengerName = self.challengerName, recipientID = self.recipientID, recipientName = self.recipientName)
+                await interaction.followup.send("Hey I see you ended up with a Reroll card there. Would you like to give me all your cards back for 5 new packs?", view=view)
+            else:
+                await interaction.followup.send(file=file_r, embed=embed_r)
+            #open packs for the user who sent the challenge
+            embed_c, file_c, rerollOpened_c, tradeOpened_c = MSSBCardGenerator.openMultiplePacks(5)
+            challenger_user = client.get_user(self.challengerID)
+            embed_c.set_author(name=challenger_user.name, icon_url=challenger_user.display_avatar.url)
+            print(rerollOpened_c, tradeOpened_c)
+            if rerollOpened_c and not tradeOpened_c:
+                await interaction.followup.send(file=file_c, embed=embed_c)
+                view = ViewRerollC(challengerID=self.challengerID, challengerName = self.challengerName, recipientID = self.recipientID, recipientName = self.recipientName)
+                await interaction.followup.send("Hey I see you ended up with a Reroll card there. Would you like to give me all your cards back for 5 new packs?", view=view)
+            elif (not rerollOpened_c) and tradeOpened_c:
+                await interaction.followup.send(file=file_c, embed=embed_c)
+            elif rerollOpened_c and tradeOpened_c:
+                await interaction.followup.send(file=file_c, embed=embed_c)
+                view = ViewRerollC(challengerID=self.challengerID, challengerName = self.challengerName, recipientID = self.recipientID, recipientName = self.recipientName)
+                await interaction.followup.send("Hey I see you ended up with a Reroll card there. Would you like to give me all your cards back for 5 new packs?", view=view)
+            else:
+                await interaction.followup.send(file=file_c, embed=embed_c)
+
+    @discord.ui.button(label="No", style=discord.ButtonStyle.danger, emoji="❌")
+    async def no_button_callback(self, interaction: discord.Interaction, button:discord.ui.button):
+        if interaction.user.id != self.recipientID:
+            await interaction.response.send_message("Hey you can't speak for them! Only the user being challenged can accept or decline the Pack Battle challenge.", ephemeral=True)
+        else:
+            await interaction.response.edit_message(content="Pack Battle Declined",view=None)
+            await interaction.followup.send("Calm down now, no Pack Battle to be had here. Maybe another time?")
+
+#command to trigger a pack battle
+@client.tree.command(name='packbattle', description='Challenege a friend to rip packs and play a baseball game!', guild=GUILD_ID)
+async def confirmParticipant(interaction: discord.Interaction, user: discord.Member):
+    #set the id's of the parties involved
+    challengerID = interaction.user.id
+    challengerName = interaction.user.name
+    recipientID = user._user.id
+    recipientName = user._user.name
+    #convert to id string's
+    challengerIDStr = str(challengerID)
+    recipientIDStr = str(recipientID)
+    recipientNameStr = str(recipientName)
+    challengeMessage, challengeConfirm = MSSBPackBattle.construct_challenge_message(challengerIDStr, recipientIDStr, recipientNameStr)
+    view = View(challengerID=challengerID, challengerName=challengerName, recipientID=recipientID, recipientName=recipientName)
+    await interaction.response.send_message(challengeMessage)
+    await interaction.followup.send(challengeConfirm, view=view)
 client.run(TOKEN)
